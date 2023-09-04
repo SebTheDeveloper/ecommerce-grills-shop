cd44595ca3385833fa2ac6252f480de078db4d22
sebdoubleu <111027905+sebdoubleu@users.noreply.github.com>
2023-09-03 22:31:51 -0400

    add branded title

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,33 @@
 export function About() {
-  return <h1>About</h1>
+  return (
+    <>
+      <div className="title-wrapper">
+        <div className="title">Palm Beach Custom Grillz</div>
+        <span style={{ marginLeft: "auto" }}>
+          grillz done right the first time.
+        </span>
+      </div>
+      <div style={{display: 'flex', flexDirection: 'column', gap: '1.5rem', fontSize: '1.25rem'}}>
+        <p>
+          At Palm Beach Custom Grillz, excellence is our promise. We're dedicated
+          to delivering top-tier grillz without breaking the bank.
+        </p>
+        <p>
+          With a spectrum spanning from NPG to luxurious 18K Gold, our
+          custom-fitted grillz cater to every style.
+        </p>
+        <p>
+          From timeless classics to daring fangs and beyond, our collection offers
+          a plethora of choices. We believe in lasting impressions, which is why
+          our deep cuts come standard, ensuring a permanent and flawless look, all
+          at no extra cost.
+        </p>
+        <p>
+          While our prices remain competitive, compromising on quality is not an
+          option. Trust us to craft your grillz to perfection the first time at
+          Palm Beach Custom Grillz.
+        </p>
+      </div>
+    </>
+  );
 }
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -7,7 +7,15 @@ import { StoreItem } from "../components/StoreItem";
 export function Store() {
   return (
     <>
-      <h1>Shop</h1>
+      <div className="title-wrapper">
+        <div className="title">Palm Beach Custom Grillz</div>
+        <span style={{ marginLeft: "auto" }}>
+          grillz done right the first time.
+        </span>
+      </div>
+      <h1 style={{ marginBottom: "1.25rem", opacity: "0.97" }} id="shop">
+        Shop
+      </h1>
       <Row xs={1} md={2} lg={3} className="g-5 g-md-4">
         {storeItems.map((item) => (
           <Col key={item.id}>
