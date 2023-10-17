8b5ca264a185f26491e8e94ba2c58364133fb48f
sebdoubleu <111027905+sebdoubleu@users.noreply.github.com>
2023-10-17 18:36:02 -0400

    update store item names

--- src/components/LogoTitle.tsx
+++ src/components/LogoTitle.tsx
@@ -3,7 +3,7 @@ export function LogoTitle() {
     <div className="title-wrapper">
       <div className="title">Palm Beach Custom Grillz</div>
       <span style={{ marginLeft: "auto" }}>
-        Grillz done right the first time.
+        grillz done right the first time.
       </span>
     </div>
   );
