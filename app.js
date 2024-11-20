bb75f3ed739a1c744f6f33ec612ec87060a55199
sebdoubleu <111027905+sebdoubleu@users.noreply.github.com>
2024-11-20 00:19:04 -0500

    update checkout pricing logic for Stripe

--- app.js
+++ app.js
@@ -61,18 +61,39 @@ app.post("/create-checkout-session", async (req, res) => {
         }
       }
 
+      let veryGoodPrice = 0;
+
+      switch (item.quantity) {
+        case 1:
+          veryGoodPrice = Number(storeItem?.prices["1"]);
+          break;
+        case 6:
+          veryGoodPrice = Number(storeItem?.prices["6"]);
+          break;
+        case 8:
+          veryGoodPrice = Number(storeItem?.prices["8"]);
+          break;
+        case 10:
+          veryGoodPrice = Number(storeItem?.prices["10"]);
+          break;
+      }
+
       // Main items
       lineItems.push({
         price_data: {
           currency: "usd",
           product_data: {
-            name: isMoldKit ? `Mold Kit | ${moldKitSelection}` : storeItem.name,
+            name: isMoldKit
+              ? `Mold Kit | ${moldKitSelection}`
+              : `${storeItem.name} (${item.quantity} ${
+                  item.quantity > 1 ? "teeth" : "tooth"
+                })`,
           },
           unit_amount: isMoldKit
             ? storeItem.prices[item.quantity || 1] * 100
-            : storeItem.prices["1"] * 100,
+            : veryGoodPrice * 100,
         },
-        quantity: isMoldKit ? 1 : item.quantity,
+        quantity: 1,
       });
 
       // Add-ons
